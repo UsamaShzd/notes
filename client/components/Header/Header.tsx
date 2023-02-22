6ee313159a0692a7f765e70c857b2b68625df72e
Usama Sehezad <usamashzd99@gmail.com>
2023-02-23 00:05:39 +0500

    Header implemented.

---
+++ client/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Header() {
+  return (
+    <Navbar bg='dark' expand='lg' className='py-3'>
+      <Container>
+        <Navbar.Brand href='#' className='text-white'>
+          Notes
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='navbarScroll' />
+        <Navbar.Collapse id='navbarScroll'>
+          <Form className='d-flex ms-lg-auto mt-2 mt-lg-0'>
+            <Form.Control
+              type='search'
+              placeholder='Search'
+              className='me-2'
+              aria-label='Search'
+            />
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
